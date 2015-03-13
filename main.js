8120b0ee1b0cb56e8350c093cd29107e149b8e2f
Thanh Ky Quan <thanhquanky@gatech.edu>
2015-03-12 21:37:24 -0400

    Add firebase

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-function main($scope) {
+
+var foodie = angular.module('foodie', ['firebase']);
+
+
+foodie.controller('MainController', ['$scope', '$firebaseObject', '$firebaseArray', function($scope, $firebaseObject, $firebaseArray) {
+    Firebase.INTERNAL.forceWebSockets();
+    var ref = new Firebase('https://amber-inferno-7229.firebaseio.com/FoodEvents');
+
+    $scope.foodEvents = new $firebaseArray(ref);
     $scope.showGetLocation = false;
     $scope.showAddLocation = false;
 
 
     $scope.addLocation = function () {
+        $scope.showGetLocation = false;
+        $scope.showAddLocation = true;
+    };
+
+    $scope.getLocation = function () {
         $scope.showGetLocation = true;
         $scope.showAddLocation = false;
     };
 
-    $scope.getLocation = function () {
-        $scope.showGetLocation = false;
-        $scope.showAddLocation = true;
-    };
-    
     $scope.submit = function(myForm) {
         var dataObject = {
             location: $scope.myForm.location,
@@ -20,5 +28,6 @@ function main($scope) {
             capacity: $scope.myForm.capacity,
             time: $scope.myForm.time
         }
+        $scope.foodEvents.$add(dataObject);
     }
-};
+}]);

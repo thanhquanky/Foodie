0dcc11c02fbbfae285a60a3c2485f247c3d25dd7
Thang Nguyen <thang911pk@gmail.com>
2015-03-12 20:10:25 -0400

    done

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-window.onload = function() {
-  document.querySelector('#greeting').innerText =
-    'Hello, World! It is ' + new Date();
+function main($scope) {
+    $scope.showGetLocation = false;
+    $scope.showAddLocation = false;
+
+
+    $scope.addLocation = function () {
+        $scope.showGetLocation = true;
+        $scope.showAddLocation = false;
+    };
+
+    $scope.getLocation = function () {
+        $scope.showGetLocation = false;
+        $scope.showAddLocation = true;
+    };
+    
+    $scope.submit = function(myForm) {
+        var dataObject = {
+            location: $scope.myForm.location,
+            food: $scope.myForm.food,
+            capacity: $scope.myForm.capacity,
+            time: $scope.myForm.time
+        }
+    }
 };
